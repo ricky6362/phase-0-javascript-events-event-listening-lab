7c7d963de3f9ca7f0a0b173274a1dda86481e82e
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-21 14:45:35 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
+// accessing the #input element
+const input = document.getElementById("input")
+
+// creating my alert function
+function sayHi(){
+    alert("I was clicked")
 }
+
+// adding the event listener that triggers the alert text.
+function addingEventListener(input) {
+    input.addEventListener("click", sayHi)
+}
+
+// invoking the function to see it on the browser
+addingEventListener(input)
